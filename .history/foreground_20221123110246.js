19f04a37ba68bc73473f52b74b96bdbb9c42603c
carlmeek <62119209+carlmeek@users.noreply.github.com>
2022-11-23 11:02:59 +0000

    push

---
+++ .history/foreground_20221123110246.js
@@ -0,0 +1,15 @@
+//Nothing here now.
+
+console.log("Truly Live INTERCEPT script TIMER!")
+setTimeout(setUserVideoWidth, 1000);
+
+function setUserVideoWidth() {
+  console.log("Truly Live INTERCEPT script RUNNING!")
+  if (document.getElementById('user-video')) {
+    if (document.getElementById('user-video')) {
+    document.getElementById('user-video').style.width='824px'
+    document.getElementById('user-video').style.height='466px'
+    document.getElementById('user-video').style.left='122px'
+    document.getElementById('user-video').style.top='15px'
+  }
+}
