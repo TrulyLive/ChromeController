176c488e2990b24d7ad2a5e4f602329482e999d8
carlmeek <62119209+carlmeek@users.noreply.github.com>
2022-11-23 10:50:43 +0000

    progress

---
+++ .history/foreground_20221123105033.js
@@ -0,0 +1,11 @@
+//Nothing here now.
+
+console.log("Truly Live INTERCEPT script!")
+
+$(document).ready(function(){ 
+  console.log("Truly Live INTERCEPT running!")
+  document.getElementById('user-video').style.width='522px'
+  document.getElementById('user-video').style.height='295px'  
+});
+
+
